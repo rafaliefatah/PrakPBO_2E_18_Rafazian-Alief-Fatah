8348f524ac4f56853701d60895113ab7b9002e27
rafaliefatah <rafazian336@gmail.com>
2025-09-07 18:48:50 +0700

    P2_Jobsheet2_Tugas3

---
+++ P2/Lingkaran.java
@@ -0,0 +1,13 @@
+package P2;
+
+public class Lingkaran {
+    public double r,phi=3.14;
+
+    public double hitungLuas() {
+        return phi*r*r;
+    }
+
+    public double hitungKeliling() {
+        return 2*phi*r;
+    }
+}
