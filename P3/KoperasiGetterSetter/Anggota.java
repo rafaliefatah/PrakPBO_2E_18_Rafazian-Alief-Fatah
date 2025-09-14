80bc8a2abdc3601b2ec16fbd2b0c52a0868a695d
rafaliefatah <rafazian336@gmail.com>
2025-09-14 10:11:20 +0700

    P3_Jobsheet3_Percobaan4

--- P3/KoperasiGetterSetter/Anggota.java
+++ P3/KoperasiGetterSetter/Anggota.java
@@ -4,6 +4,12 @@ public class Anggota {
     private String nama,alamat;
     private float simpanan;
 
+    Anggota(String nama, String alamat) {
+        this.nama=nama;
+        this.alamat=alamat;
+        this.simpanan=0;
+    }
+
     public void setNama(String nama) {
         this.nama=nama;
     }
