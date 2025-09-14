d0aed96a76202c431625385474f28af9b4c395fe
rafaliefatah <rafazian336@gmail.com>
2025-09-14 09:34:32 +0700

    P3_Jobsheet3_Percobaan2

--- P3/MotorEncapsulation/Motor.java
+++ P3/MotorEncapsulation/Motor.java
@@ -1,8 +1,38 @@
 package MotorEncapsulation;
 
 public class Motor {
-    public int kecepatan=0;
-    public boolean kontakOn=false;
+    private int kecepatan=0;
+    private boolean kontakOn=false;
+    private int maksKecepatan=100;
+
+    public void nyalakanMesin() {
+        kontakOn=true;
+    }
+    
+    public void matikanMesin() {
+        kontakOn=false;
+        kecepatan=0;
+    }
+
+    public void tambahKecepatan() {
+        if (kontakOn==true) {
+            if (kecepatan==maksKecepatan) {
+                System.out.println("Kecepatan sudah maksimal!");
+            } else {
+                kecepatan+=5;
+            }
+        } else {
+            System.out.println("Kecepatan tidak bisa bertambah karena Mesin Off!\n");
+        }
+    }
+
+    public void kurangiKecepatan() {
+        if (kontakOn==true) {
+            kecepatan-=5;
+        } else {
+            System.out.println("Kecepatan tidak bisa berkurang karena Mesin Off!\n");
+        }
+    }
 
     public void printStatus() {
         if (kontakOn==true) {
