1fb807ca1fd1f80678461642c6de50d9d60c1831
rafaliefatah <rafazian336@gmail.com>
2025-09-20 15:51:10 +0700

    P4_Jobsheet4_Percobaan1

---
+++ P4/jti.polinema.relasiclass.percobaan1/Laptop.java
@@ -0,0 +1,33 @@
+public class Laptop {
+    private String merk;
+    private Processor proc;
+
+    Laptop () {
+    }
+
+    Laptop (String merk, Processor proc) {
+        this.merk=merk;
+        this.proc=proc;
+    }
+
+    public void info() {
+        System.out.println("Merk Laptop = "+merk);
+        proc.info();
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public Processor getProc() {
+        return proc;
+    }
+
+    public void setProc(Processor proc) {
+        this.proc = proc;
+    }
+}
